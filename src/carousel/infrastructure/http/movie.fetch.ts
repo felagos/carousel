ec533c2a11187d67f49c09d59eb80dc4f113aca3
Felipe Lagos <felagossanchez@gmail.com>
2025-04-19 20:37:03 -0400

    feat: remove MovieFetchService and integrate fetching logic directly in MovieFetch

--- src/carousel/infrastructure/http/movie.fetch.ts
+++ src/carousel/infrastructure/http/movie.fetch.ts
@@ -1,13 +1,12 @@
 import { MovieFetchPort } from "../../domain/out/movie-fetch.port";
-import { MovieDetail } from "../../domain/movie.domain";
-import { MovieFetchService } from "../http/movie-fetch.service";
+import { Movie, MovieDetail } from "../../domain/movie.domain";
+import apiClient from "../../../common/api/http.config";
 
 export class MovieFetch implements MovieFetchPort {
 
-	constructor(private movieFetch: MovieFetchService) {}
-
 	fetchMovies = async (): Promise<MovieDetail[]> => {
-		return this.movieFetch.fetchMovies();
+		const response = await apiClient.get<Movie>('/movie/now_playing?language=en-US&page=1');
+		return response.data.results;
 	}
 
 }
--- src/carousel/infrastructure/ui/Carousel.tsx
+++ src/carousel/infrastructure/ui/Carousel.tsx
@@ -1,10 +1,9 @@
 import { useCarousel } from "../../application/hooks/useCarousel";
-import { MovieFetchService } from "../http/movie-fetch.service";
 import { MovieFetch } from "../http/movie.fetch";
 import { MovieRepository } from "../repository/movie-repository";
 import { MovieStoreRepository } from "../repository/movie-store.repository";
 
-const movieAdapter = new MovieFetch(new MovieFetchService());
+const movieAdapter = new MovieFetch();
 const movieRepository = new MovieRepository(new MovieStoreRepository());
 
 export const Carousel = () => {
