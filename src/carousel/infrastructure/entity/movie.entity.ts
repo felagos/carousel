c7398a6d10813cf937389c50445978bebb23b987
Felipe Lagos <felagossanchez@gmail.com>
2025-04-19 23:16:05 -0400

    feat: refactor movie data models to use classes and add constructors for better instantiation

--- src/carousel/infrastructure/entity/movie.entity.ts
+++ src/carousel/infrastructure/entity/movie.entity.ts
@@ -1,55 +1,36 @@
 /**
- * Main class representing movie data response from API
+ * Main interface representing movie data response from API
  * Contains pagination information and an array of movie details
  */
-export class Movie {
+export interface MovieEntity {
   /** Date range for the current movie results */
-  dates: Dates
+  dates: DatesEntity
   /** Current page number of results */
   page: number
   /** Array of individual movie information */
-  results: MovieDetail[]
+  results: MovieDetailEntity[]
   /** Total available pages of results */
   total_pages: number
   /** Total count of movies matching the criteria */
   total_results: number
-
-  constructor(
-    dates: Dates,
-    page: number,
-    results: MovieDetail[],
-    total_pages: number,
-    total_results: number
-  ) {
-    this.dates = dates;
-    this.page = page;
-    this.results = results;
-    this.total_pages = total_pages;
-    this.total_results = total_results;
-  }
 }
 
 /**
  * Represents date boundaries for movie results
  * Often used for filtering content by release dates
  */
-export class Dates {
+export interface DatesEntity {
   /** Latest date in the date range */
   maximum: string
   /** Earliest date in the date range */
   minimum: string
-
-  constructor(maximum: string, minimum: string) {
-    this.maximum = maximum;
-    this.minimum = minimum;
-  }
 }
 
 /**
  * Detailed information about an individual movie
  * Contains all essential metadata for displaying movie information
  */
-export class MovieDetail {
+export interface MovieDetailEntity {
   /** Indicates if the movie is adults-only content */
   adult: boolean
   /** Path to the backdrop image */
@@ -78,36 +59,4 @@ export class MovieDetail {
   vote_average: number
   /** Number of votes that have been cast */
   vote_count: number
-
-  constructor(
-    adult: boolean,
-    backdrop_path: string,
-    genre_ids: number[],
-    id: number,
-    original_language: string,
-    original_title: string,
-    overview: string,
-    popularity: number,
-    poster_path: string,
-    release_date: string,
-    title: string,
-    video: boolean,
-    vote_average: number,
-    vote_count: number
-  ) {
-    this.adult = adult;
-    this.backdrop_path = backdrop_path;
-    this.genre_ids = genre_ids;
-    this.id = id;
-    this.original_language = original_language;
-    this.original_title = original_title;
-    this.overview = overview;
-    this.popularity = popularity;
-    this.poster_path = poster_path;
-    this.release_date = release_date;
-    this.title = title;
-    this.video = video;
-    this.vote_average = vote_average;
-    this.vote_count = vote_count;
-  }
 }
--- src/carousel/infrastructure/http/movie.fetch.ts
+++ src/carousel/infrastructure/http/movie.fetch.ts
@@ -1,14 +1,32 @@
 import { MovieFetchPort } from "../../domain/ports/outgoing/movie-fetch.port";
-import { Movie, MovieDetail } from "../../domain/movie.domain";
+import { MovieDetail } from "../../domain/movie.domain";
 import { HttpClient } from "../../../common/api/http.config";
+import { MovieEntity } from "../entity/movie.entity";
 
 export class MovieFetch implements MovieFetchPort {
 
 	constructor(private httpClient: HttpClient) { }
 
 	fetchMovies = async (): Promise<MovieDetail[]> => {
-		const response = await this.httpClient.get<Movie>('/movie/now_playing?language=en-US&page=1');
-		return response.data.results;
+		const response = await this.httpClient.get<MovieEntity>('/movie/now_playing?language=en-US&page=1');
+
+		// Convert each MovieDetailEntity to a MovieDetail class instance
+		return response.data.results.map(movie => new MovieDetail(
+			movie.adult,
+			movie.backdrop_path,
+			movie.genre_ids,
+			movie.id,
+			movie.original_language,
+			movie.original_title,
+			movie.overview,
+			movie.popularity,
+			movie.poster_path,
+			movie.release_date,
+			movie.title,
+			movie.video,
+			movie.vote_average,
+			movie.vote_count
+		));
 	}
 
 }
